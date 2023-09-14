f7b7b5138567348908ab7c7c8cc3656830aea306
platonkl <kulishivan48@gmail.com>
2023-09-14 19:46:13 +0700

    Добавил подключение к дб, надо делать миграции

--- ForumDdd/Controllers/HomeController.cs
+++ ForumDdd/Controllers/HomeController.cs
@@ -6,13 +6,6 @@ namespace ForumDdd.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Recently()
         {
             return View();
@@ -26,14 +19,6 @@ namespace ForumDdd.Controllers
         public IActionResult WithoutAnswer()
         {
             return View();
-        }
-
-
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ ForumDdd/Data/ForumDddContext.cs
@@ -0,0 +1,22 @@
+﻿using ForumDdd.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumDdd.Data
+{
+    public class ForumDddContext : DbContext
+    {
+
+        public ForumDddContext(DbContextOptions<ForumDddContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+
+}
