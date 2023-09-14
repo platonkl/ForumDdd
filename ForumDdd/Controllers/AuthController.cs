508564b5512d90f1dd2a0c151284935ba4d2cc32
platonkl <kulishivan48@gmail.com>
2023-09-14 22:25:13 +0700

    dbConnection2

--- ForumDdd/Controllers/AuthController.cs
+++ ForumDdd/Controllers/AuthController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ForumDdd.Data;
+using ForumDdd.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ForumDdd.Controllers
 {
     public class AuthController : Controller
     {
+
+        ForumDddDbContext _context;
+        public AuthController(ForumDddDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult SignUp()
         {
             return View();
@@ -13,5 +22,14 @@ namespace ForumDdd.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
     }
 }
